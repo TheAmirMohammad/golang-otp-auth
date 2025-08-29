f4afc64aae7580185005873bc96ac79ae08a689b
Amir Mohammad <sohrabi2557@gmail.com>
2025-08-29 13:45:02 +0330

    chore(init): minimal fiber server with /health

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
 func main() {
-	
+	app := fiber.New()
+	app.Get("/health", func(c *fiber.Ctx) error { return c.SendString("OK") })
+	log.Println("listening on :8080")
+	if err := app.Listen(":8080"); err != nil { log.Fatal(err) }
 }
