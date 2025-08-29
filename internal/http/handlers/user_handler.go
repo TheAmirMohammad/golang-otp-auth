c6a0c23b1228eae4864d68d61e2fc8775a6457b7
Amir Mohammad <sohrabi2557@gmail.com>
2025-08-29 18:00:56 +0330

    feat(http/users): add get user func

--- internal/http/handlers/user_handler.go
+++ internal/http/handlers/user_handler.go
@@ -1,5 +1,19 @@
 package handlers
 
-import "github.com/TheAmirMohammad/otp-service/internal/domain/user"
+import (
+	"context"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+
+	"github.com/TheAmirMohammad/otp-service/internal/domain/user"
+)
 
 type UserHandler struct { Users user.Repository }
+
+func (h *UserHandler) GetUser(c *fiber.Ctx) error {
+	id := c.Params("id")
+	u, _ := h.Users.GetByID(context.Background(), id)
+	if u == nil { return c.Status(http.StatusNotFound).JSON(fiber.Map{"error":"not found"}) }
+	return c.JSON(u)
+}
