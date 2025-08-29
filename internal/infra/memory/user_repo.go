afc3acbb29120ec6a1df9f9064e07cb6e46927a7
Amir Mohammad <sohrabi2557@gmail.com>
2025-08-29 14:21:19 +0330

    feat(repo/memory): add in-memory User repository
    add create

--- internal/infra/memory/user_repo.go
+++ internal/infra/memory/user_repo.go
@@ -1,7 +1,11 @@
 package memory
 
 import (
+	"context"
 	"sync"
+	"time"
+
+	"github.com/google/uuid"
 
 	"github.com/TheAmirMohammad/otp-service/internal/domain/user"
 )
@@ -14,4 +18,13 @@ type UserRepo struct {
 
 func NewUserRepo() *UserRepo {
 	return &UserRepo{byID: map[string]user.User{}, byPhone: map[string]string{}}
+}
+
+func (r *UserRepo) Create(ctx context.Context, u *user.User) error {
+	r.mu.Lock(); defer r.mu.Unlock()
+	if u.ID == "" { u.ID = uuid.NewString() }
+	if u.RegisteredAt.IsZero() { u.RegisteredAt = time.Now().UTC() }
+	r.byID[u.ID] = *u
+	r.byPhone[u.Phone] = u.ID
+	return nil
 }
