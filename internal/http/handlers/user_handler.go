85e5ace1ace37aa3e9190d615f804fc806730ac8
Amir Mohammad <sohrabi2557@gmail.com>
2025-08-30 00:26:47 +0330

    docs(swagger): add annotate user endpoints

--- internal/http/handlers/user_handler.go
+++ internal/http/handlers/user_handler.go
@@ -19,6 +19,15 @@ type listResp struct {
 	Size  int         `json:"size"`
 }
 
+// GetUser godoc
+// @Summary   Get single user by ID
+// @Tags      users
+// @Produce   json
+// @Param     id path string true "User ID"
+// @Success   200 {object} user.User
+// @Failure   404 {object} map[string]string
+// @Security  Bearer
+// @Router    /users/{id} [get]
 func (h *UserHandler) GetUser(c *fiber.Ctx) error {
 	id := c.Params("id")
 	u, _ := h.Users.GetByID(context.Background(), id)
@@ -26,6 +35,16 @@ func (h *UserHandler) GetUser(c *fiber.Ctx) error {
 	return c.JSON(u)
 }
 
+// ListUsers godoc
+// @Summary   List users with pagination & search
+// @Tags      users
+// @Produce   json
+// @Param     page   query int    false "Page (1-based)" minimum(1) default(1)
+// @Param     size   query int    false "Page size" minimum(1) maximum(100) default(20)
+// @Param     search query string false "Search by phone"
+// @Success   200 {object} listResp
+// @Security  Bearer
+// @Router    /users [get]
 func (h *UserHandler) ListUsers(c *fiber.Ctx) error {
 	page, _ := strconv.Atoi(c.Query("page", "1"))
 	size, _ := strconv.Atoi(c.Query("size", "20"))
