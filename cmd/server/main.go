973b6448d9d5dfc4ffe8e6f22844a659e6573f1f
Amir Mohammad <sohrabi2557@gmail.com>
2025-08-29 16:21:11 +0330

    feat(http/auth): add router

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,12 +6,16 @@ import (
 	"github.com/gofiber/fiber/v2"
 
 	"github.com/TheAmirMohammad/otp-service/internal/config"
+	httpapi "github.com/TheAmirMohammad/otp-service/internal/http"
 )
 
 func main() {
 	cfg := config.Load()
 	app := fiber.New()
+
 	app.Get("/health", func(c *fiber.Ctx) error { return c.SendString("OK") })
+	httpapi.New(app) //router
+
 	log.Printf("listening on :%s", cfg.Port)
 	if err := app.Listen(":" + cfg.Port); err != nil { log.Fatal(err) }
 }
---
+++ internal/http/router.go
@@ -0,0 +1,9 @@
+package httpapi
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func New(app *fiber.App) {
+	// api := app.Group("/api/v1")
+}
