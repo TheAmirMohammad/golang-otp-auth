3aac2f89440199409f0cdb6273f0f2d47f534e4a
Amir Mohammad <sohrabi2557@gmail.com>
2025-08-29 18:13:48 +0330

    feat(http/users): add user list func

--- internal/http/handlers/user_handler.go
+++ internal/http/handlers/user_handler.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"context"
 	"net/http"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 
@@ -11,9 +12,27 @@ import (
 
 type UserHandler struct { Users user.Repository }
 
+type listResp struct {
+	Items []user.User `json:"items"`
+	Total int         `json:"total"`
+	Page  int         `json:"page"`
+	Size  int         `json:"size"`
+}
+
 func (h *UserHandler) GetUser(c *fiber.Ctx) error {
 	id := c.Params("id")
 	u, _ := h.Users.GetByID(context.Background(), id)
 	if u == nil { return c.Status(http.StatusNotFound).JSON(fiber.Map{"error":"not found"}) }
 	return c.JSON(u)
+}
+
+func (h *UserHandler) ListUsers(c *fiber.Ctx) error {
+	page, _ := strconv.Atoi(c.Query("page", "1"))
+	size, _ := strconv.Atoi(c.Query("size", "20"))
+	search := c.Query("search", "")
+	items, total, err := h.Users.List(context.Background(), user.ListFilter{
+		Search: search, Limit: size, Offset: (page-1)*size,
+	})
+	if err != nil { return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error":"list error"}) }
+	return c.JSON(listResp{Items: items, Total: total, Page: page, Size: size})
 }
