428bf8fdfdf66089ae34dd171eb6340ba26870cf
Amir Mohammad <sohrabi2557@gmail.com>
2025-08-29 16:08:55 +0330

    feat(http/auth): add request-otp
    with error handling

--- internal/http/handlers/auth_handler.go
+++ internal/http/handlers/auth_handler.go
@@ -1,9 +1,11 @@
 package handlers
 
 import (
+	"net/http"
 	"regexp"
 
 	"github.com/TheAmirMohammad/otp-service/internal/otp"
+	"github.com/gofiber/fiber/v2"
 )
 
 type AuthHandler struct {
@@ -14,3 +16,20 @@ type AuthHandler struct {
 type requestOTPReq struct { Phone string `json:"phone"` }
 
 var phoneRx = regexp.MustCompile(`^[0-9+\-() ]{5,20}$`) // For Iran numbers it should be "^09\d{9}$"
+
+func (h *AuthHandler) RequestOTP(c *fiber.Ctx) error {
+	var req requestOTPReq
+	if err := c.BodyParser(&req); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error":"invalid body"})
+	}
+	if !phoneRx.MatchString(req.Phone) {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error":"invalid phone"})
+	}
+	if !h.Limiter.Allow(req.Phone) {
+		return c.Status(http.StatusTooManyRequests).JSON(fiber.Map{"error":"rate limit: 3 per 10 minutes"})
+	}
+	if _, err := h.OTP.Generate(req.Phone); err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error":"otp error"})
+	}
+	return c.JSON(fiber.Map{"message":"otp generated (check server logs)"})
+}
