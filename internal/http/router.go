152aaae27a2278b9e68ffdd719ec80981f91b68a
Amir Mohammad <sohrabi2557@gmail.com>
2025-08-29 23:09:38 +0330

    docs(swagger): add global meta (title/basepath/security)

--- internal/http/router.go
+++ internal/http/router.go
@@ -2,8 +2,10 @@ package httpapi
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/swagger"
 )
 
 func New(app *fiber.App) {
 	// api := app.Group("/api/v1")
+	app.Get("/swagger/*", swagger.HandlerDefault)
 }
