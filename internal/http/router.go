9a6ccec5a59442e8fcdde03c4ad11588b2ae9b8b
Amir Mohammad <sohrabi2557@gmail.com>
2025-08-29 16:39:53 +0330

    feat(http/auth): add request-otp endpoint with rate limit

--- internal/http/router.go
+++ internal/http/router.go
@@ -2,8 +2,11 @@ package httpapi
 
 import (
 	"github.com/gofiber/fiber/v2"
+
+	"github.com/TheAmirMohammad/otp-service/internal/http/handlers"
 )
 
-func New(app *fiber.App) {
-	// api := app.Group("/api/v1")
+func New(app *fiber.App, ah *handlers.AuthHandler) {
+	api := app.Group("/api/v1")
+	api.Post("/auth/request-otp", ah.RequestOTP)
 }
